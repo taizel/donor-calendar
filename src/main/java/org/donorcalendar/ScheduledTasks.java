a56ed644103d34da843721717a5e5bbccfe3a29f
Táizel Girão Martins <taizel@users.noreply.github.com>
2017-06-06 23:12:38 +0100

    Add scheduled task to update users status

--- src/main/java/org/donorcalendar/ScheduledTasks.java
+++ src/main/java/org/donorcalendar/ScheduledTasks.java
@@ -1,5 +1,6 @@
 package org.donorcalendar;
 
+import org.donorcalendar.domain.UserStatus;
 import org.donorcalendar.persistence.UserProfileEntity;
 import org.donorcalendar.persistence.UserProfileRepository;
 import org.slf4j.Logger;
@@ -7,6 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Component
 public class ScheduledTasks {
@@ -22,11 +26,25 @@ public class ScheduledTasks {
 
     @Scheduled(fixedRate = 10000)
     public void sendEmailToRememberDonors() {
-
         for (UserProfileEntity user : userProfileRepository.findUsersToRemind()) {
             log.info("Sent reminder for user: " + user.getName());
             user.setNextReminder(user.getNextReminder().plusDays(user.getDaysBetweenReminders()));
             userProfileRepository.save(user);
+        }
+    }
+
+    @Scheduled(fixedRate = 15000)
+    public void updateUsersStatus() {
+        for (UserProfileEntity user : userProfileRepository.findAll()) {
+            UserStatus currentStatus = user.getUserStatus();
+            long daysSinceLastDonation = ChronoUnit.DAYS.between(user.getLastDonation(), LocalDate.now());
+            UserStatus newStatus = UserStatus.getStatusByNumberOfDaysSinceLastDonation(daysSinceLastDonation);
+            if (currentStatus != newStatus) {
+                log.info("User status changed: " + user.getName());
+                user.setUserStatus(newStatus);
+                userProfileRepository.save(user);
+            }
+
         }
     }
 }
