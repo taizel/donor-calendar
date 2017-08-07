7cf41ce66d6ac83fe16af225a8062218ba16a672
Táizel Girão Martins <taizel@users.noreply.github.com>
2017-08-07 11:30:15 +0100

    Update deprecated annotations

--- src/main/java/org/donorcalendar/web/DonorZoneController.java
+++ src/main/java/org/donorcalendar/web/DonorZoneController.java
@@ -6,7 +6,7 @@ import org.donorcalendar.security.UserAuthenticationDetails;
 import org.donorcalendar.service.UserService;
 import org.donorcalendar.web.dto.ShoppingFacilities;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/org/donorcalendar/web/UserController.java
+++ src/main/java/org/donorcalendar/web/UserController.java
@@ -13,7 +13,7 @@ import org.donorcalendar.web.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
