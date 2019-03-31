3376fda638cc0e501a5086ab29d73f89e5306cdb
Táizel Girão Martins <taizel@users.noreply.github.com>
2019-03-31 20:26:39 +0200

    Improve protection against reflected XSS attacks
    Issues detected by Sonar.

--- app/src/main/java/org/donorcalendar/web/ControllerAdvice.java
+++ app/src/main/java/org/donorcalendar/web/ControllerAdvice.java
@@ -6,6 +6,7 @@ import org.donorcalendar.web.dto.ClientErrorInformationDto;
 import org.donorcalendar.model.ForbiddenAccessException;
 import org.donorcalendar.model.NotFoundException;
 import org.donorcalendar.model.ValidationException;
+import org.owasp.encoder.Encode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,27 +18,32 @@ import javax.servlet.http.HttpServletRequest;
 public class ControllerAdvice {
     private final Log logger = LogFactory.getLog(this.getClass());
 
+    private ClientErrorInformationDto buildClientErrorInformationDto(HttpServletRequest req, String message) {
+        return new ClientErrorInformationDto(message,
+                Encode.forHtml(req.getRequestURI()), req.getMethod());
+    }
+
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ClientErrorInformationDto> handleValidationError(HttpServletRequest req, ValidationException e) {
-        ClientErrorInformationDto error = new ClientErrorInformationDto(e.getMessage(), req.getRequestURI(), req.getMethod());
+        ClientErrorInformationDto error = buildClientErrorInformationDto(req, e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ForbiddenAccessException.class)
     public ResponseEntity<ClientErrorInformationDto> handleForbiddenAccessError(HttpServletRequest req, ForbiddenAccessException e) {
-        ClientErrorInformationDto error = new ClientErrorInformationDto(e.getMessage(), req.getRequestURI(), req.getMethod());
+        ClientErrorInformationDto error = buildClientErrorInformationDto(req, e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ClientErrorInformationDto> handleNotFoundError(HttpServletRequest req, NotFoundException e) {
-        ClientErrorInformationDto error = new ClientErrorInformationDto(e.getMessage(), req.getRequestURI(), req.getMethod());
+        ClientErrorInformationDto error = buildClientErrorInformationDto(req, e.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ClientErrorInformationDto> handleInternalServerError(HttpServletRequest req, Throwable t) {
-        ClientErrorInformationDto error = new ClientErrorInformationDto("Unexpected internal error.", req.getRequestURI(), req.getMethod());
+        ClientErrorInformationDto error = buildClientErrorInformationDto(req, "Unexpected internal error.");
         logger.error("Unexpected internal error.", t);
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
