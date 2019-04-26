a1659e12a73fa9ec9e6d931071d538cfd4ab4d7d
Táizel Girão Martins <taizel@users.noreply.github.com>
2019-04-26 07:02:23 +0200

    Improve unit test for DonorZoneController

--- webapp/src/test/java/org/donorcalendar/rest/DonorZoneControllerTest.java
+++ webapp/src/test/java/org/donorcalendar/rest/DonorZoneControllerTest.java
@@ -1,39 +1,43 @@
 package org.donorcalendar.rest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import org.donorcalendar.model.ForbiddenAccessException;
 import org.donorcalendar.model.UserProfile;
 import org.donorcalendar.model.UserStatus;
 import org.donorcalendar.security.UserSecurityDetails;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class DonorZoneControllerTest {
 
-	private DonorZoneController target = new DonorZoneController();
+    private DonorZoneController target = new DonorZoneController();
 
-	@Test
-	public void testGetShoppingFacilitiesAccessAllowed() throws ForbiddenAccessException {
-		UserProfile userProfile = new UserProfile();
-		userProfile.setUserStatus(UserStatus.DONOR);
-		UserSecurityDetails userSecurityDetails = new UserSecurityDetails(userProfile, null);
+    @Test
+    public void testGetShoppingFacilitiesAccessAllowed() {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setUserStatus(UserStatus.DONOR);
+        UserSecurityDetails userSecurityDetails = new UserSecurityDetails(userProfile, null);
 
-		target.getShoppingFacilities(userSecurityDetails);
-	}
+        try {
+            target.getShoppingFacilities(userSecurityDetails);
+        } catch (ForbiddenAccessException e) {
+            fail();
+        }
+    }
 
-	@Test
-	public void testGetShoppingFacilitiesForbiddenAccess() {
-		UserProfile userProfile = new UserProfile();
-		userProfile.setUserStatus(UserStatus.NEED_TO_DONATE);
-		UserSecurityDetails userSecurityDetails = new UserSecurityDetails(userProfile, null);
+    @Test
+    public void testGetShoppingFacilitiesForbiddenAccess() {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setUserStatus(UserStatus.NEED_TO_DONATE);
+        UserSecurityDetails userSecurityDetails = new UserSecurityDetails(userProfile, null);
 
-		try {
-			target.getShoppingFacilities(userSecurityDetails);
-			fail();
-		} catch (ForbiddenAccessException e) {
-			assertEquals("You need to be in the status \"Donor\"or \"Potential Donor\" to access this resource.", e.getMessage());
-		}
-	}
+        try {
+            target.getShoppingFacilities(userSecurityDetails);
+            fail();
+        } catch (ForbiddenAccessException e) {
+            assertEquals("You need to be in the status \"Donor\"or \"Potential Donor\" to access this resource.", e.getMessage());
+        }
+    }
 
 }
