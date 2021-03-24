7c3d4349ab452ec3ddd1bcc3d3847a703009f0c4
Táizel Girão Martins <taizel@users.noreply.github.com>
2021-03-24 17:30:03 +0100

    Fix raw type code smell issue on Sonar

--- persistence/src/main/java/org/donorcalendar/config/HerokuPersistenceConfig.java
+++ persistence/src/main/java/org/donorcalendar/config/HerokuPersistenceConfig.java
@@ -23,11 +23,10 @@ public class HerokuPersistenceConfig {
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(dbUrl);
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-
-        return dataSourceBuilder.build();
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .build();
     }
 }
